0f9652f29ec4fd73f5e9b6a8f405d0511109244d
hawkobe <sanders.jms@gmail.com>
2024-01-07 15:41:32 -0700

    set up board length constant, SVG for array definition

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,19 @@
 class Board
+  BOARD_LENGTH = 8
+
+  attr_reader :positions_matrix
 
   def initialize
-    @positions = Array.new(8) { Array.new(8, '-')}
+    @positions_matrix = Array.new(BOARD_LENGTH) { Array.new(BOARD_LENGTH)}
   end
   
+  def [](square)
+    row, column = square
+    positions_matrix[row][column]
+  end
+
+  def []=(square, piece)
+    row, column = square
+    positions_matrix[row][column] = piece
+  end
 end
