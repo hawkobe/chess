96d143ee9872b1d360ff63124ef0ca02e98c3456
Jacob Sanders <sanders.jms@gmail.com>
2024-01-08 21:54:04 -0700

    refactored #print_board, defined helper methods #print_row and #print_walls

--- lib/board/board_printer.rb
+++ lib/board/board_printer.rb
@@ -1,18 +1,35 @@
 class BoardPrinter 
-  SQUARE = '[ ]'
+  FIRST_FLOOR = '+--------+'
+  FLOOR = '--------+'
+  FIRST_WALL = '|        |'
+  WALLS = '        |'
+  SQUARE_HEIGHT = 3
 
-  attr_reader :board_length
+  attr_reader :board_length, :board
 
-  def initialize(board_length) 
-    @board_length = board_length
+  def initialize(board) 
+    @board = board
+    @board_length = board.class::BOARD_LENGTH
   end
 
   def print_board
-    board_length.times do |row|
-      board_length.times do |column|
-        column < 7 ? print(SQUARE) : puts(SQUARE)
-      end
-    end
+    print_floor
+    SQUARE_HEIGHT.times { print_row }
+    print_floor
+  end
+
+  def clear_screen
+    system 'clear'
+  end
+
+  private
+
+  def print_floor
+    puts FIRST_FLOOR + FLOOR * (board_length - 1)
+  end
+
+  def print_row
+    puts FIRST_WALL + WALLS * (board_length - 1)
   end
 
 end
