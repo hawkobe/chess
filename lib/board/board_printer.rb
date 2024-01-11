02e5b8a6e009e0263102611203c8ca3ea1df150d
Jacob Sanders <sanders.jms@gmail.com>
2024-01-10 20:15:57 -0700

    added margin to board, added column letters, split row into white row and black row

--- lib/board/board_printer.rb
+++ lib/board/board_printer.rb
@@ -3,6 +3,8 @@ require 'colorize'
 class BoardPrinter 
   FIRST_FLOOR = '+--------+'
   FLOOR = '--------+'
+  COLUMN_LETTERS = *('A'..'H')
+  LEFT_MARGIN = " " * 3
 
   FIRST_ROW_BLACK = '|        |'
   FIRST_ROW_WHITE = "|████████|"
@@ -19,11 +21,13 @@ class BoardPrinter
   end
 
   def print_board
+    print_column_letters
     print_floor
     board_length.times do |row_number|
       SQUARE_HEIGHT.times { print_row(row_number) }
       print_floor
     end
+    print_column_letters
   end
 
   def clear_screen
@@ -33,16 +37,31 @@ class BoardPrinter
   private
 
   def print_floor
-    puts FIRST_FLOOR + FLOOR * (board_length - 1)
+    puts LEFT_MARGIN + FIRST_FLOOR + FLOOR * (board_length - 1)
   end
 
   def print_row(row_number)
-    puts row_number.even? ? row : row.reverse
+    puts row_number.even? ? white_starting_row : black_starting_row
   end
 
-  def row
-    FIRST_ROW_WHITE + 
+  def white_starting_row
+    LEFT_MARGIN +
+      FIRST_ROW_WHITE +
       (BLACK_ROW + WHITE_ROW) * 3 +
       BLACK_ROW
+  end
+  
+  def black_starting_row
+    LEFT_MARGIN +
+      FIRST_ROW_BLACK +
+      (WHITE_ROW + BLACK_ROW) * 3 +
+      WHITE_ROW
+  end
+
+  
+  def print_column_letters
+    COLUMN_LETTERS.each_with_index do |letter, index|
+      index < 7 ? print("        #{letter}") : puts("        #{letter}")
+    end
   end
 end
