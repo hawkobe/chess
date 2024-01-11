f5d49fb453901b7f54e37e256ef992cd6d4ac6b8
Jacob Sanders <sanders.jms@gmail.com>
2024-01-10 21:13:26 -0700

    added row numbers, refactored black and white starting row methods to capture row numbers

--- lib/board/board_printer.rb
+++ lib/board/board_printer.rb
@@ -1,11 +1,12 @@
 require 'colorize'
 
 class BoardPrinter 
-  FIRST_FLOOR = '+--------+'
-  FLOOR = '--------+'
   COLUMN_LETTERS = *('A'..'H')
+  ROW_NUMBERS = [*('1'..'8')].reverse
   LEFT_MARGIN = " " * 3
 
+  FIRST_FLOOR = '+--------+'
+  FLOOR = '--------+'
   FIRST_ROW_BLACK = '|        |'
   FIRST_ROW_WHITE = "|████████|"
   BLACK_ROW = '        |'
@@ -24,9 +25,10 @@ class BoardPrinter
     print_column_letters
     print_floor
     board_length.times do |row_number|
-      SQUARE_HEIGHT.times { print_row(row_number) }
+      SQUARE_HEIGHT.times { |square_row| print_row(row_number, square_row) }
       print_floor
     end
+    new_line
     print_column_letters
   end
 
@@ -40,28 +42,47 @@ class BoardPrinter
     puts LEFT_MARGIN + FIRST_FLOOR + FLOOR * (board_length - 1)
   end
 
-  def print_row(row_number)
-    puts row_number.even? ? white_starting_row : black_starting_row
+  def print_row(row_number, row_in_square)
+    puts row_number.even? ? white_starting_row(row_number, row_in_square) : black_starting_row(row_number, row_in_square)
   end
 
-  def white_starting_row
-    LEFT_MARGIN +
-      FIRST_ROW_WHITE +
-      (BLACK_ROW + WHITE_ROW) * 3 +
-      BLACK_ROW
+  def white_starting_row(row_number, row_in_square)
+    if row_in_square == 1
+      "#{ROW_NUMBERS[row_number]}  " +
+        FIRST_ROW_WHITE +
+        (BLACK_ROW + WHITE_ROW) * 3 +
+        BLACK_ROW +
+        "  #{ROW_NUMBERS[row_number]}"
+    else
+      LEFT_MARGIN +
+        FIRST_ROW_WHITE +
+        (BLACK_ROW + WHITE_ROW) * 3 +
+        BLACK_ROW
+    end
   end
   
-  def black_starting_row
-    LEFT_MARGIN +
+  def black_starting_row(row_number, row_in_square)
+    if row_in_square == 1
+      "#{ROW_NUMBERS[row_number]}  " +
+        FIRST_ROW_BLACK +
+        (WHITE_ROW + BLACK_ROW) * 3 +
+        WHITE_ROW +
+        "  #{ROW_NUMBERS[row_number]}"
+    else
+      LEFT_MARGIN +
       FIRST_ROW_BLACK +
       (WHITE_ROW + BLACK_ROW) * 3 +
       WHITE_ROW
+    end
   end
 
-  
   def print_column_letters
-    COLUMN_LETTERS.each_with_index do |letter, index|
-      index < 7 ? print("        #{letter}") : puts("        #{letter}")
-    end
+    COLUMN_LETTERS.each { |letter| print "        #{letter}" }
+    new_line
+    new_line
+  end
+
+  def new_line(amount_of_lines=1)
+    amount_of_lines.times { puts '' }
   end
 end
