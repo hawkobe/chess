b0f25fcaa64a474878c12a968a65d512c3d0d6bb
Jacob Sanders <sanders.jms@gmail.com>
2024-01-16 00:09:01 -0700

    defined #populate_pawns and #populate_pieces methods

--- lib/board/board_general.rb
+++ lib/board/board_general.rb
@@ -1,5 +1,10 @@
+require_relative '../piece.rb'
+
 class Board
   BOARD_LENGTH = 8
+  PIECE_SEQUENCE = [
+    Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook
+  ]
 
   attr_reader :positions_matrix
 
@@ -33,5 +38,25 @@ class Board
   def square_empty?(square)
     row, column = square
     in_bounds?(square) && positions_matrix[row][column].nil?
+  end
+
+  def populate_pawns
+    [1, 6].each do |pawn_row|
+      color = pawn_row == 1 ? :black : :white
+
+      BOARD_LENGTH.times do |column|
+        self[[pawn_row, column]] = Pawn.new([pawn_row, column], color)
+      end
+    end
+  end
+
+  def populate_pieces
+    [0, 7].each do |pieces_row|
+      color = pieces_row == 0 ? :black : :white
+
+      PIECE_SEQUENCE.each_with_index do |piece, index|
+        self[[pieces_row, index]] = piece.new([pieces_row, index], color)
+      end
+    end
   end
 end
