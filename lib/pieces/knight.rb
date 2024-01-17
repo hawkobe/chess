42efd7a8121e15b3859e3234229bea8bdca913ae
Jacob Sanders <sanders.jms@gmail.com>
2024-01-17 03:11:52 -0700

    defined initialize, defined #find_all_moves, #valid_empty_square_moves and #valid_capture_moves

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,4 +1,35 @@
 class Knight < Piece
   WHITE = '♘' 
   BLACK = "♞"
+
+  def initialize(location, color, board)
+    super
+  end
+
+  def find_all_moves
+    row = location[0]
+    column = location[1]
+    moves = []
+    (-2..2).each do |i|
+      next if i.zero?
+
+      j = i.abs == 2 ? 1 : 2
+      moves << [row - j, column + i]
+      moves << [row + j, column + i]
+    end
+    moves
+  end
+
+  def valid_empty_square_moves
+    legal_moves = find_all_moves.select do |move| 
+      move if board.square_empty?(move)
+    end
+  end
+
+  def valid_capture_moves
+    legal_moves = find_all_moves.select do |move|
+      next if board[move] == ' '
+      move if board.in_bounds?(move) && board.piece_color_in_square(move) != board[location].color
+    end
+  end
 end
