48e61249faeef27edd4901d8decc768707adf9b2
Jacob Sanders <sanders.jms@gmail.com>
2024-01-20 22:56:20 -0700

    refactored class to include MOVES constant, refactored find_all_moves and find_capture_moves to compensate. Changed original find_all_moves to find_every_move

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,33 +1,34 @@
+require 'pry-byebug'
+
 class Knight < Piece
   WHITE = '♘' 
   BLACK = "♞"
+
+  MOVES = [[2, 1], [1, 2], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
 
   def initialize(location, color, board)
     super
   end
 
-  def find_all_moves
+  def find_every_move
     row = location[0]
     column = location[1]
-    moves = []
-    (-2..2).each do |i|
-      next if i.zero?
+    all_moves = []
 
-      j = i.abs == 2 ? 1 : 2
-      moves << [row - j, column + i]
-      moves << [row + j, column + i]
+    MOVES.each do |move|
+      all_moves << [row + move[0], column + move[1]]
     end
-    moves
+    all_moves
   end
 
-  def find_empty_square_moves
-    @valid_moves = find_all_moves.select do |move| 
+  def find_all_moves
+    @valid_moves = find_every_move.select do |move| 
       move if board.square_empty?(move)
     end
   end
 
   def find_capture_moves
-    @valid_capture_moves = find_all_moves.select do |move|
+    @valid_capture_moves = find_every_move.select do |move|
       next if board[move] == ' '
       move if board.in_bounds?(move) && board.piece_color_in_square(move) != board[location].color
     end
