1f3d94ae9b20966799321e2c3268010f641c17d7
Jacob Sanders <sanders.jms@gmail.com>
2024-01-20 22:48:54 -0700

    defined #initialize and set up MOVES

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,4 +1,18 @@
+require_relative 'bishop.rb'
+require_relative 'rook.rb'
+require_relative 'knight.rb'
+
 class Queen < Piece
   WHITE = '♕'
   BLACK = "♛"
+
+  MOVES = [
+    Bishop::MOVES,
+    Rook::MOVES,
+  ].flatten(1)
+
+  def initialize(location, color, board)
+    super
+  end
+
 end
