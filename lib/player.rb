8ad3937c0b209fdc3d2bb0b82d227e72e6257fe6
Jacob Sanders <sanders.jms@gmail.com>
2024-01-31 23:23:33 -0700

    defined #initialize, #select_piece, #select_move, #convert_player_input, #piece_input_valid?, and #move_input_valid?

---
+++ lib/player.rb
@@ -0,0 +1,54 @@
+require 'pry-byebug'
+
+class Player 
+  attr_reader :color, :name, :board
+
+  def initialize(name, color, board)
+    @name = name
+    @color = color
+    @board = board
+  end
+
+  def select_piece
+    puts "Alright, #{name}, it's your turn. Please select a piece you would like to move"
+    input = gets.chomp
+    until piece_input_valid?(convert_player_input(input))
+      puts "oops, that input doesn't work. Try again"
+      input = gets.chomp
+    end
+    puts "okay so you've selected #{input}. Great!"
+    board.currently_selected_piece = board[convert_player_input(input)]
+    input
+  end
+
+  def select_move
+    puts "Okay, now where would you like to move?"
+    input = gets.chomp
+    until move_input_valid?(convert_player_input(input))
+      puts "oops, that input doesn't work. Try again"
+      input = gets.chomp
+    end
+    puts "okay you'd like to move to #{input}"
+    input
+  end
+
+  def convert_player_input(input)
+    [board.class::MOVE_HASH[input[1]], board.class::MOVE_HASH[input[0]]]
+  end
+
+  def piece_input_valid?(converted_input)
+    board.in_bounds?(converted_input) && 
+    board[converted_input].is_a?(Piece) && 
+    board[converted_input].color == self.color &&
+    board[converted_input].has_valid_moves?
+  end
+
+  def move_input_valid?(converted_input)
+    board.currently_selected_piece.valid_moves.include?(converted_input) || board.currently_selected_piece.valid_captures.include?(converted_input)
+  end
+
+  def to_s
+    color
+  end
+
+end
